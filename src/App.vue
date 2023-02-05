ac11df4afd339e7d81db09f758a6fe4a89a0445d
ethan <51106797+ethanxo@users.noreply.github.com>
2023-02-05 14:36:00 -0500

    push

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <CardControls />
+  <div id="cards" class="cards">
+    <Card />
+  </div>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+import Card from './components/Card.vue'
+import CardControls from './components/CardControls.vue'
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap');
+
+.cards {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.button {
+  background-color: #2895e8; /* Blue */
+  border: none;
+  color: white;
+  padding: 8px 2vw;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  text-decoration: none;
+  font-size: 14px;
+  margin-bottom: 8px;
+  cursor: pointer;
+  border-radius: 4px;
+  width: 25%;
+  transform: scale(1);
+  transition: 0.6s;
+}
+.button:hover, .button:focus {
+  transform: scale(1.05);
+  box-shadow: 0px 0px 48px 10px rgba(40,149,232,.7);
+}
+
+@media (min-width: 501px) and (max-width: 800px) {
+  .card>.button {
+    display: inline-block;
+  }
+}
+@media (max-width: 500px) {
+  .card {
+    transform: scale(1.1);
+  }
 }
 </style>
